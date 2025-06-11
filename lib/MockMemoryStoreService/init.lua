a631f583c0a8ab84f57512d38c1df43fd74a77a0
Theo <finchasaurus@gmail.com>
2025-06-10 19:06:17 -0600

    Hash Map largely implemented

--- lib/MockMemoryStoreService/init.lua
+++ lib/MockMemoryStoreService/init.lua
@@ -1,13 +1,15 @@
-local MockMemoryStoreSortedMap = require(script.MockMemoryStoreSortedMap)
+local MockMemoryStoreHashMap = require(script.MockMemoryStoreHashMap)
 local MockMemoryStoreQueue = require(script.MockMemoryStoreQueue)
 local MockMemoryStoreQuota = require(script.MockMemoryStoreQuota)
+local MockMemoryStoreSortedMap = require(script.MockMemoryStoreSortedMap)
 local MockMemoryStoreUtils = require(script.MockMemoryStoreUtils)
 
 local RunService = game:GetService("RunService")
 
 local MockMemoryStoreService = {
-    queues = {},
-    sortedMaps = {}
+	queues = {},
+	sortedMaps = {},
+	hashMaps = {},
 }
 MockMemoryStoreService.__index = MockMemoryStoreService
 
@@ -15,48 +17,60 @@ MockMemoryStoreService.__index = MockMemoryStoreService
     Will retrieve a MockMemoryStoreSortedMap object under the specified name
 ]]
 function MockMemoryStoreService:GetSortedMap(name: string)
-    local sortedMap = self.sortedMaps[name];
-    if sortedMap == nil then
-        sortedMap = MockMemoryStoreSortedMap.new(name)
-        self.sortedMaps[name] = sortedMap
-    end
+	local sortedMap = self.sortedMaps[name]
+	if sortedMap == nil then
+		sortedMap = MockMemoryStoreSortedMap.new(name)
+		self.sortedMaps[name] = sortedMap
+	end
 
-    return sortedMap
+	return sortedMap
 end
 
 --[[
     Will retrieve a MockMemoryStoreQueue object under the specified name
 ]]
 function MockMemoryStoreService:GetQueue(name: string, timeout: number?)
-    MockMemoryStoreUtils.WarnOnce("MockMemoryStoreService queue is still in development, and may not work accurately just yet.")
+	MockMemoryStoreUtils.WarnOnce(
+		"MockMemoryStoreService queue is still in development, and may not work accurately just yet."
+	)
 
-    local queue = self.queues[name]
-    if queue == nil then
-        queue = MockMemoryStoreQueue.new(name, timeout)
-        self.queues[name] = queue
-    end
+	local queue = self.queues[name]
+	if queue == nil then
+		queue = MockMemoryStoreQueue.new(name, timeout)
+		self.queues[name] = queue
+	end
 
-    return queue
+	return queue
+end
+
+function MockMemoryStoreService:GetHashMap(name: string)
+	local hashMap = self.hashMaps[name]
+	if hashMap == nil then
+		hashMap = MockMemoryStoreHashMap.new()
+		self.hashMaps[name] = hashMap
+	end
+
+	return hashMap
 end
 
 -- Lifetime handling
 local function onHeartbeat(deltaTime)
-    MockMemoryStoreQuota:UpdateQuota()
+	MockMemoryStoreQuota:UpdateQuota()
 
-    -- Need to handle expiration for each map value
-    for _, map in pairs(MockMemoryStoreService.sortedMaps) do
-        -- Iterate through each value to check if they're expired
-        -- Then expire them
-        for key, value in pairs(map.mapValues) do
-            if value.expiration <= tick() then
-                map:RemoveExpiringKey(key)
-            end
-        end
-    end
+	-- Need to handle expiration for each map value
+	for _, map in pairs(MockMemoryStoreService.sortedMaps) do
+		-- Iterate through each value to check if they're expired
+		-- Then expire them
+		for key, value in pairs(map.mapValues) do
+			if value.expiration <= tick() then
+				map:RemoveExpiringKey(key)
+			end
+		end
+	end
 
-    for _, queue in pairs(MockMemoryStoreService.queues) do
-        queue:HandleTimeouts()
-    end
+	for _, queue in pairs(MockMemoryStoreService.queues) do
+		queue:HandleTimeouts()
+	end
 end
 RunService.Heartbeat:Connect(onHeartbeat)
 
